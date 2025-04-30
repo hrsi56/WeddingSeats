ceeceeef65035e547a9095959cec8ef699050b4b
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-04-30 16:08:20 +0300

    ועעליה איזה פחד

--- WeddingSeats.py
+++ WeddingSeats.py
@@ -256,7 +256,7 @@ elif 'מוזמן' in st.session_state:
                                                 st.session_state['stopstate'] = False
                                             else:
                                                 st.warning(
-                                                    f"בחר {st.session_state['num_guests']} כיסאות.")
+                                                    f"בחר רק {st.session_state['num_guests']}.")
                                                 st.session_state['stopstate'] = True
 
                                         if not checked and is_sel:
