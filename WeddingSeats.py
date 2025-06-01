285378bb191f9b44b2a74320e9dd1d6cff553ffc
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-06-01 10:47:45 +0300

    ועעליה איזה פחד

--- WeddingSeats.py
+++ WeddingSeats.py
@@ -1000,7 +1000,7 @@ st.markdown("""
     <div style="text-align: center; margin-top: 20px;">
         <a href="https://photos.app.goo.gl/CXuHxit6c9J6rypy8" target="_blank">
             <img src="https://www.gizchina.com/wp-content/uploads/images/2025/02/Google-photos.png"
-                 alt="Google Photos" style="width: 600px; border-radius: 12px; box-shadow: 0px 4px 4px #dbc9bd;">
+                 alt="Google Photos" style="width: 600px; border-radius: 12px; box-shadow: 0 4px 4px #dbc9bd;">
         </a>
     </div>
 """, unsafe_allow_html=True)
